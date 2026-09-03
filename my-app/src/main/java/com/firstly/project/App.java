5da37af9583bf3b6d5f29c2df0146639c1219dfb
root <root@ip-172-31-32-152.ec2.internal>
2026-09-03 08:13:11 +0000

    maven.

---
+++ my-app/src/main/java/com/firstly/project/App.java
@@ -0,0 +1,13 @@
+package com.firstly.project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
